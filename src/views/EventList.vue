3f57778033d374134c1b789d571737f0151d71cf
Justynasd <justynasd@gmail.com>
2020-12-06 15:23:12 +0200

    adding json file as a server

---
+++ src/views/EventList.vue
@@ -0,0 +1,56 @@
+<template>
+  <h1>Events for You</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import EventCard from "@/components/EventCard.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "EventList",
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 1123,
+          category: "x-wing",
+          subcategory: "extended",
+          title: "Store champinship",
+          description:
+            "New Store championship event, good time and great prizes... come and check by yourself!!!",
+          location: "Your local store",
+          date: "2021-01-15",
+          time: "10:00",
+          organizer: "Mr. Orginizer",
+        },
+        {
+          id: 2123,
+          category: "x-wing",
+          subcategory: "hyperspace",
+          title: "Store champinship",
+          description:
+            "New Store championship event, good time and great prizes... come and check by yourself!!!",
+          location: "Your local store",
+          date: "2021-02-15",
+          time: "10:00",
+          organizer: "Mr. Orginizer",
+        },
+      ]
+    };
+  },
+});
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
